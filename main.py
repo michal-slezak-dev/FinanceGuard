bc9070239ab13622949ad4fe699845f9b1de7d18
Michał Ślęzak <realslezak.michal@gmail.com>
2024-08-25 18:48:42 +0200

    Almost finish the homepage

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap4
+from forms import ContactForm
+from datetime import date
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '495bd4f2b03184706c74ad11eb8a2a0a'
+Bootstrap4(app)
+
+current_year = date.today().year
+
+
+@app.route("/login")
+def login():
+    return "hej"
+
+
+@app.route("/register")
+def register():
+    return "hej"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", year=current_year)
+
+
+@app.route("/features")
+def features():
+    return render_template("features.html", year=current_year)
+
+
+@app.route("/contact")
+def contact():
+    contact_form = ContactForm()
+    return render_template("contact.html", year=current_year, form=contact_form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
