9021e3602ed7a54af88cfc81a0a7dd246e825537
Michał Ślęzak <realslezak.michal@gmail.com>
2024-08-26 19:47:34 +0200

    update contact.py - fix utf-8 mail content issue

--- contact.py
+++ contact.py
@@ -1,5 +1,6 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email import charset
 from mailjet_rest import Client
 from dotenv import load_dotenv
 import os
@@ -11,13 +12,15 @@ SECRET_API_KEY = os.getenv('MAILJET_SECRET_API_KEY')
 
 
 def send_mail(username, message, from_email, user_email, to_email):
+    charset.add_charset('utf-8', charset.SHORTEST, charset.QP)
+
     msg = MIMEMultipart()
     msg['Subject'] = f"{username} - Contact Response"
     msg['From'] = user_email
     msg['To'] = to_email
 
     # attach the email content
-    msg.attach(MIMEText(message + f"\n\nMessage sent from {user_email}", 'plain'))
+    msg.attach(MIMEText(message + f"\n\nMessage sent by {username} from {user_email}", 'plain'))
 
     mailjet = Client(auth=(API_KEY, SECRET_API_KEY), version='v3.1')
 
